24edad2781adc32d051ea005c9c071ce79dbb651
tplatina <jiajunta@andrew.cmu.edu>
2019-01-26 16:33:18 -0500

    Item Mechanic

---
+++ project/Assets/Scripts/LootItemManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootItemManager : MonoBehaviour {
+
+    public static LootItemManager instance { get; private set; }
+
+    [SerializeField]
+    private int[] itemNums = new int[8];
+
+    [SerializeField]
+    private GameObject[] itemPrefabs = new GameObject[8];
+
+    private List<GameObject> itemPool; 
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    public void StartGame () {
+		for(int i = 0; i < 8; i++)
+        {
+            List<LevelCoord> tmpset = LevelData.instance.GetPlayerSpawnPoint(i + 1);
+            if (tmpset.Count < itemNums[i])
+            {
+                Debug.Log("Error: Uncompatible count for id: " + i + 1);
+                break;
+            }
+            for (int j = itemNums[i]; j < tmpset.Count; j++)
+            {
+                int k = Random.Range(0, j - 1);
+                if (k < itemNums[i])
+                {
+                    LevelCoord tmpCoord = tmpset[j];
+                    tmpset[j] = tmpset[k];
+                    tmpset[k] = tmpCoord;
+                }
+            }
+            for(int j = 0; j < tmpset.Count; j++)
+            {
+                if (j < itemNums[i])
+                {
+                    var tmpPrefab = Instantiate(itemPrefabs[i]);
+                    tmpPrefab.transform.position = LevelData.instance.Coord2WorldPos(tmpset[j]);
+                    itemPool.Add(tmpPrefab);
+                }
+                else
+                {
+                    LevelData.instance.OriginMap[tmpset[j].y, tmpset[j].x] = -1;
+                    LevelData.instance.GridMap[tmpset[j].y, tmpset[j].x] = -1;
+                }
+            }
+        }
+	}
+	
+    public void ItemPickup(LevelCoord coord)
+    {
+        for(int i = 0; i < itemPool.Count; i++)
+        {
+            if(LevelData.instance.WorldPos2Coord(itemPool[i].transform.position) == coord)
+            {
+                Destroy(itemPool[i]);
+                itemPool.RemoveAt(i);
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private bool hasItem;
+
+	// Use this for initialization
+	void Start () {
+        hasItem = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PickupItem(LevelCoord coord)
+    {
+        if (!hasItem)
+        {
+            hasItem = true;
+            int grid = LevelData.instance.GridMap[coord.y, coord.x] - (int)GridType.LootSpawn1 + 1;
+            LootItemManager.instance.ItemPickup(coord);
+            LevelData.instance.OriginMap[coord.y, coord.x] = -1;
+        }
+    }
+}
