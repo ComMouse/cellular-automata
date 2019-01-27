1a2516ddf398a328214a8e9ed91d23b30ad90cf3
Kevin Han <hanyikai1996@126.com>
2019-01-27 07:04:23 -0500

    Add skeleton for preparation controllers

---
+++ project/Assets/Scripts/AvatarController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarController : MonoBehaviour
+{
+    [SerializeField]
+    private InputController inputCtrl;
+
+    public AvatarState state = AvatarState.None;
+
+    private void Start()
+    {
+        Debug.Assert(inputCtrl != null);
+        state = AvatarState.Prepare;
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case AvatarState.Prepare:
+                if (inputCtrl.GetButton1Down())
+                {
+                    state = AvatarState.Ready;
+                    // Play confirm sound
+                    // Update ready UI
+                }
+                break;
+            case AvatarState.Ready:
+                break;
+        }
+    }
+
+    public void Reset()
+    {
+        state = AvatarState.Prepare;
+
+        // TODO
+    }
+}
+
+public enum AvatarState
+{
+    None = 0,
+    Prepare = 1,
+    Ready = 2,
+}
